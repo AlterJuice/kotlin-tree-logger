a249a049563c40c040700640b193d8c62cc6ef6c
Alterjuice <alterjuice1758@gamil.com>
2025-03-09 03:40:53 +0200

    Updated gradle

--- treelogger/build.gradle.kts
+++ treelogger/build.gradle.kts
@@ -1,24 +1,34 @@
 plugins {
-    id("java-library")
+    id("java")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     id("maven-publish")
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
 }
+
+group = "com.github.AlterJuice"
+version = "1.0.2"
+
+//
+//dependencies {
+//    // Add any external dependencies your library needs
+//    implementation(kotlin("stdlib-jdk8"))
+//}
+
 kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+    jvmToolchain(11)
 }
 publishing {
     publications {
         create<MavenPublication>("release") {
             from(components["java"])
-            groupId = "com.alterjuice.utils"
-            artifactId = "treelogger"
-            version = "1.0.0"
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
         }
     }
 }
